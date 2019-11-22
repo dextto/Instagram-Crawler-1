833cf99b6576457be0d1b74ab33877439a4c5d12
dextto <zd.gary@gmail.com>
2019-11-22 15:43:49 +0900

    같은 좋아요는 같은 grade로 분류되도록 함.

--- classify_images.py
+++ classify_images.py
@@ -16,19 +16,68 @@ def main(path):
         file_list.append(splited)
 
     file_list = sorted(file_list, key=operator.itemgetter(2, 3))
+    num = int(len(file_list) / 10)
+
+    likes = [f[2] for f in file_list]
 
     for f in file_list:
         f[2] = str(f[2])
 
-    n = int(len(file_list) / 10)
+    grades = []
+    for i in range(9):
+        grades.append(likes[i * num:(i + 1) * num])
+    grades.append(likes[num * 9:])
+
+    print(len(file_list))
+    print(len(grades))
+    temp = sum(grades, [])
+    print(len(temp))
+
     for i in range(10):
+        if i == 9:
+            break
+        p_grade = grades[i]
+        n_grade = grades[i+1]
+        p_last = p_grade[-1]
+        n_first = grades[i+1][1]
+        if p_last != n_first:
+            continue
+
+        num = p_last
+        p_cnt = p_grade.count(num)
+        n_cnt = grades[i+1].count(num)
+
+        if p_cnt < n_cnt:
+            p_grade = p_grade[0:-p_cnt]
+            for _ in range(p_cnt):
+                n_grade.append(num)
+        elif p_cnt > n_cnt:
+            for _ in range(n_cnt):
+                p_grade.append(num)
+            n_grade = n_grade[n_cnt:]
+        else:
+            cnt = p_cnt
+            if len(p_grade) <= len(n_grade):
+                for _ in range(cnt):
+                    p_grade.append(num)
+                n_grade = n_grade[cnt:]
+            else:
+                p_grade = p_grade[:-cnt]
+                for _ in range(cnt):
+                    n_grade.append(num)
+    print(grades)
+
+    print(file_list)
+    _sum = 0
+    for i in range(len(grades)):
         grade_path = f'{path}/grade{str("%02d" % i)}'
         if not os.path.isdir(grade_path):
             os.mkdir(grade_path)
 
-        cnt = n if i != 9 else int(len(file_list) - n * 9)
-        for c in range(cnt):
-            shutil.copy(f'{path}/{"".join(file_list[i * n + c])}', grade_path)
+        grade = grades[i]
+        for ii in range(len(grade)):
+            shutil.copy(f'{path}/{"".join(file_list[_sum])}', grade_path)
+            _sum = _sum + 1
 
     # make_zip(path)
 
